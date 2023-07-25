a4e16d2ca8052f05eb0f47cda88bc3ad7e7dbf43
Passemoildev <joffrey.somon@gmail.com>
2023-07-25 12:14:33 +0200

    mise en page Home et footer
    Création des fichiers .scss pour les composants

--- src/components/Body/gallery/cards.js
+++ src/components/Body/gallery/cards.js
@@ -3,8 +3,8 @@ import React from'react'
 function Card ({image, title}){
     return(
         <div className='card'>
-            <img src={image} alt= {title} className='card_img'/>
-            <h2 className='card_title'>{title}</h2>
+            <img src={image} alt= {title} className='card__img'/>
+            <h2 className='card__title'>{title}</h2>
         </div>
     )
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,10 +4,14 @@ import logo_black from '../../assets/logo/LOGO_black.png'
 const Footer =()=>{
     return(
     <footer className="footer">
-        <img src={logo_black} alt="logo" className="footer__logo" />
-        <p className="footer__copyright">
-          © 2020 Kasa. All rights reserved
-        </p>
+        <div className='footer__contLogo'>
+          <img src={logo_black} alt="logo" className="footer__contLogo__logo" />
+        </div> 
+        <div className='footer__contCopyright'>
+          <p className="footer__contCopyright__copyright">
+            © 2020 Kasa. All rights reserved
+          </p>
+        </div>
       </footer>
     );
 }
